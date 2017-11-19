dbe897794735f122bf9f0d97a012d5e5b9fee44a
Emil Christiansen <emil.christiansen@gmail.com>
2017-11-19 20:45:08 +0100

    changed time

--- js/scripts.js
+++ js/scripts.js
@@ -29,7 +29,7 @@ $(document).ready(function() {
 $(document).ready(function() {
 	"use strict";
 	$("#countdown").countdown({
-		date: "21 november 2017 9:00:00", /** Enter new date here **/
+		date: "21 november 2017 8:00:00", /** Enter new date here **/
 		format: "off"
 	},
 	function() {
